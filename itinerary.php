222a5998d91f6e8b4eca3fa10f93852dbfaa087e
pjullygomez <pjullygomez@gmail.com>
2022-05-19 14:43:50 +0800

    added itinerary page

---
+++ itinerary.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Biglaan Ph</title>
+
+    <!-- font awesome cdn link  -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+    />
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css" />
+
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css"
+    />
+
+    <!-- custom js file link  -->
+    <script src="js/script.js" defer></script>
+  </head>
+  <body>
+    <!-- header section starts  -->
+
+    <header class="header">
+      <div id="menu-btn" class="fas fa-bars"></div>
+
+      <a data-aos="zoom-in-left" data-aos-delay="150" href="#" class="logo">
+        <i class="fas fa-paper-plane"></i> Biglaan
+        <span style="color: #29d9d5">PH</span></a
+      >
+
+      <nav class="navbar">
+        <a data-aos="zoom-in-left" data-aos-delay="300" href="#home">home</a>
+        <a data-aos="zoom-in-left" data-aos-delay="450" href="#about">about</a>
+        <a data-aos="zoom-in-left" data-aos-delay="600" href="#destination"
+          >destination</a
+        >
+        <a data-aos="zoom-in-left" data-aos-delay="750" href="#services"
+          >services</a
+        >
+        <a data-aos="zoom-in-left" data-aos-delay="900" href="#gallery"
+          >gallery</a
+        >
+        <a data-aos="zoom-in-left" data-aos-delay="1150" href="#blogs">blogs</a>
+      </nav>
+
+      <a
+        data-aos="zoom-in-left"
+        data-aos-delay="1300"
+        href="#book-form"
+        class="btn"
+        >book now</a
+      >
+    </header>
+
+    <!-- header section ends -->
+    <!-- Itinerary -->
+    <div class="itinerary">
+      <div class="heading">
+        <span>Your travel itinerary</span>
+        <h1>To the world of an incredible vacation.</h1>
+      </div>
+      <div class="itineraryContainer">
+        <i class="fas fa-arrow-right"></i> Places To Visit
+        <br />
+        <br />
+
+        <h3>Travel Spot 1</h3>
+        <br />
+        <br />
+        <p>
+          The word “damili” translates to “the art of pottery making” in the
+          Ilocano language and is the root word for the name Red Clay
+          Pagdamilian, a popular pottery studio and shop that showcases local
+          talent with beautiful pottery and other souvenir items for sale. But
+          more than the pottery on display, what makes it a popular attraction
+          is that for a small fee, anyone can handle the clay and mold their
+          very own one-of-a-kind souvenir clay item from scratch.
+          <br />
+          <br />
+          The hands-on pottery lesson for visitors is such a big hit that it
+          became the focal point of the shop, with local and foreign visitors
+          flocking to the studio to create their own vessels and other pottery
+          designs. It became a venue for visitors to release their artistic
+          talents or a a creative outlet for participants to express their
+          personality. <br />
+          <br />
+          The clay can be molded into useful shapes with a practical purposes or
+          into decorative items to be displayed around the house such as clay
+          pots, food storage, and mugs, to name a few. The pottery and souvenir
+          shop can be found along the National Highway on Barangay La Union.
+        </p>
+        <br />
+        <h3>Travel Spot 2</h3>
+        <br />
+        <br />
+        <p>
+          Urbiztondo Beach in San Juan is known as the Surfing Capital of
+          Northern Philippines, with surfing seasons typically falling July to
+          October and November to March. This area of La Union has a reputation
+          for having the country’s most consistent waves, and thriving surf
+          culture has flourished in recent years. Many places offer surfing
+          lessons for all levels, and numerous places of lodging can also be
+          found throughout the coastline.<br /><br />
+          Surfboards are available for rent throughout the area as well.
+          Beginners typically start having lessons in Bacnotan because the waves
+          in the area are not too big, which is perfect for novice learners or
+          anyone who have never tried surfing before. The more advanced surfers
+          usually go to Urbiztondo and the neighboring town of Mona Lisa Point.
+          Restaurants offering local and international fare along with shops
+          selling local handicrafts and souvenir items are also nearby.<br /><br />
+          Urbiztondo has a great selection of restaurants, with cuisines ranging
+          from Filipino delicacies, comfort food, Mexican, Asian, and Italian
+          food. With its laidback vibe and artistic flair, local and foreign
+          visitors have been coming back to Urbiztondo every year to enjoy the
+          beach life.
+        </p>
+        <br />
+        <i class="fas fa-arrow-right"></i> Where to Stay
+        <br />
+        <br />
+
+        <h3>Hotel 1</h3>
+        <br />
+        <br />
+        <p>
+          From 400-year-old fortresses to centuries-old churches, La Union has
+          its fair share of national historical treasures that make this coastal
+          surf town a wonderful place to visit. It is also a place where
+          talented artists thrive, with pottery makers sharing their space with
+          curious visitors who want to try their hand in shaping clay. Museums
+          and spaces that inspire art are abundant among the La Union tourists
+          spots featured in this list.<br /><br />
+          Residents of La Union have managed to set up a unique agri-tourism
+          industry like grape-picking that continue to attract local and foreign
+          visitors. For many urban dwellers and Luzon residents, La Union offers
+          some of the best beaches and surfing destinations near Manila.<br /><br />
+          Because it is just a bus or car ride away from Manila, it offers a
+          quick escape from the pressures of city living. The scenic beaches,
+          Philippine resorts, and historical landmarks make it the ideal tourist
+          destination for all ages.
+        </p>
+        <br />
+        <h3>Hotel 2</h3>
+        <br />
+        <br />
+        <p>
+          The word “damili” translates to “the art of pottery making” in the
+          Ilocano language and is the root word for the name Red Clay
+          Pagdamilian, a popular pottery studio and shop that showcases local
+          talent with beautiful pottery and other souvenir items for sale. But
+          more than the pottery on display, what makes it a popular attraction
+          is that for a small fee, anyone can handle the clay and mold their
+          very own one-of-a-kind souvenir clay item from scratch.
+          <br />
+          <br />
+          The hands-on pottery lesson for visitors is such a big hit that it
+          became the focal point of the shop, with local and foreign visitors
+          flocking to the studio to create their own vessels and other pottery
+          designs. It became a venue for visitors to release their artistic
+          talents or a a creative outlet for participants to express their
+          personality. <br />
+          <br />
+          The clay can be molded into useful shapes with a practical purposes or
+          into decorative items to be displayed around the house such as clay
+          pots, food storage, and mugs, to name a few. The pottery and souvenir
+          shop can be found along the National Highway on Barangay La Union.
+        </p>
+      </div>
+      <div class="btns">
+        <a href="#book-form" class="Itinerarybtn">book now</a>
+        <a href="#index" class="Itinerarybtn">Go back</a>
+      </div>
+    </div>
+
+    <!-- gallery section starts  -->
+
+    <section class="gallery" id="gallery">
+      <div class="heading">
+        <span>Places to See</span>
+        <h1>Travel more, discover more.</h1>
+      </div>
+
+      <div class="box-container">
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="150">
+          <img src="images/itinerarygalleryimage1.jpg" alt="" />
+          <span>travel spot</span>
+          <h3>Luzon</h3>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="300">
+          <img src="images/itinerarygalleryimage2.jpg" alt="" />
+          <span>travel spot</span>
+          <h3>Luzon</h3>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="450">
+          <img src="images/itinerarygalleryimage3.jpg" alt="" />
+          <span>travel spot</span>
+          <h3>Luzon</h3>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="150">
+          <img src="images/itinerarygalleryimage4.webp" alt="" />
+          <span>travel spot</span>
+          <h3>Visayas</h3>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="300">
+          <img src="images/itinerarygalleryimage5.webp" alt="" />
+          <span>travel spot</span>
+          <h3>Luzon</h3>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="450">
+          <img src="images/itinerarygalleryimage6.jpg" alt="" />
+          <span>travel spot</span>
+          <h3>Mindanao</h3>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="150">
+          <img src="images/itinerarygalleryimage7.jpg" alt="" />
+          <span>travel spot</span>
+          <h3>Visayas</h3>
+        </div>
+
+        <div class="box" data-aos="zoom-in-up" data-aos-delay="300">
+          <img src="images/itinerarygalleryimage8.webp" alt="" />
+          <span>travel spot</span>
+          <h3>Mindanao</h3>
+        </div>
+      </div>
+    </section>
+
+    <!-- gallery section ends -->
+
+    <!-- banner section starts  -->
+
+    <div class="banner">
+      <div class="content" data-aos="zoom-in-up" data-aos-delay="300">
+        <span>start your adventures</span>
+        <h3>Let's Explore This World</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+          voluptatum praesentium amet quibusdam quam officia suscipit odio.
+        </p>
+        <a href="#book-form" class="btn">book now</a>
+      </div>
+    </div>
+
+    <!-- banner section ends -->
+
+    <!-- footer section starts  -->
+
+    <section class="footer">
+      <div class="box-container">
+        <div class="box" data-aos="fade-up" data-aos-delay="150">
+          <a href="#" class="logo">
+            <i class="fas fa-paper-plane"></i>travel
+          </a>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+            ad?
+          </p>
+          <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+          </div>
+        </div>
+
+        <div class="box" data-aos="fade-up" data-aos-delay="300">
+          <h3>quick links</h3>
+          <a href="#home" class="links">
+            <i class="fas fa-arrow-right"></i> home
+          </a>
+          <a href="#about" class="links">
+            <i class="fas fa-arrow-right"></i> about
+          </a>
+          <a href="#destination" class="links">
+            <i class="fas fa-arrow-right"></i> destination
+          </a>
+          <a href="#services" class="links">
+            <i class="fas fa-arrow-right"></i> services
+          </a>
+          <a href="#gallery" class="links">
+            <i class="fas fa-arrow-right"></i> gallery
+          </a>
+          <a href="#blogs" class="links">
+            <i class="fas fa-arrow-right"></i> blogs
+          </a>
+        </div>
+
+        <div class="box" data-aos="fade-up" data-aos-delay="450">
+          <h3>contact info</h3>
+          <p><i class="fas fa-map"></i> Lipa, Batangas</p>
+          <p><i class="fas fa-phone"></i> +63 9082447247</p>
+          <p><i class="fas fa-envelope"></i> sample@gmail.com</p>
+          <p><i class="fas fa-clock"></i> 7:00am - 10:00pm</p>
+        </div>
+
+        <div class="box" data-aos="fade-up" data-aos-delay="600">
+          <h3>newsletter</h3>
+          <p>subscribe for latest updates</p>
+          <form action="">
+            <input
+              type="email"
+              name=""
+              placeholder="enter your email"
+              class="email"
+              id=""
+            />
+            <input type="submit" value="subscribe" class="btn" />
+          </form>
+        </div>
+      </div>
+    </section>
+
+    <div class="credit">
+      created by <span>Group 2</span> | all rights reserved!
+    </div>
+
+    <!-- footer section ends -->
+    <!-- footer section ends -->
+
+    <!-- addComment Edrian -->
+    <!-- comment -->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+
+    <script>
+      AOS.init({
+        duration: 800,
+        offset: 150,
+      });
+    </script>
+  </body>
+</html>
