920061f5028fe7f76c14f45f9c9e883f0c61816a
edrian0512 <mr.edrian@gmail.com>
2022-05-24 16:50:58 +0800

    Fix Form

--- it.php
+++ it.php
@@ -92,11 +92,11 @@
             </div>
         </section>
 
-<!-- end -->
-<!-- start form -->
+        <!-- end -->
+        <!-- start form -->
         <div class="container" id='margin'>
             <div class="row">
-                <div class="col-lg-9">
+                <div class="col-lg-8">
                     <div id="image" class="carousel slide" data-bs-ride="carousel">
 
                         <div class="carousel-indicators ">
@@ -135,170 +135,171 @@
 
                 <!-- form -->
 
-                <div class="col-lg-3">
-                    <div class="form mt-5 d-lg-flex">
+                <div class="col-lg-4">
+                    <div class=" mt-5 ">
                         <form action="">
-                            <h3>Book this Tour</h3>
-                            <div class="textemail">
-                                <div>
-                                    <input type="text" placeholder="Name" id='boxIT'>
-                                </div>
-                                <div>
-                                    <input type="email" placeholder="Email" id='boxIT'>
-                                </div>
-                                <div>
-                                    <input type="number" class="width" placeholder="Contact Number" id='boxIT'>
-                                </div>
+                            <h3 class='text-white'>Book this Tour</h3>
+                            <div class='form-outline'>
+                                <input type="text" placeholder="Name" id='boxIT' class='form-control form-control-lg'>
                             </div>
-                            <div class="adultchild">
-                                <div>
-                                    <input type="number" class="nwidth" placeholder="adult" id='boxIT'>
-                                </div>
-                                <div>
-                                    <input type="number" class="nwidth" placeholder="child" id='boxIT'>
-                                </div>
+
+                            <div>
+                                <input type="email" placeholder="Email" id='boxIT' class='form-control form-control-lg'>
                             </div>
-                            <div class="check">
-                                <div class="checkin">
-                                    <label>Check-In</label>
-                                    <input type="date" placeholder="mm/dd/yyyy" id='boxIT'>
-                                </div>
-                                <div class="checkout">
-                                    <label>Check-Out</label>
-                                    <input type="date" placeholder="mm/dd/yyyy" id='boxIT'>
-                                </div>
-                            </div>
-                            <div class="textarea">
-                                <textarea name="Inquiry" cols="42" rows="3"></textarea>
+
+                            <div>
+                                <input type="number" placeholder="Contact Number" id='boxIT'
+                                    class='form-control form-control-lg'>
                             </div>
                             <div>
-                                <button class="fbtn">Inquire Now</button>
+                                <input type="number" placeholder="adult" id='boxIT'
+                                    class='form-control form-control-lg'>
                             </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-
-        <div class='container mt-4'>
-            <div class="container" id='margin'>
-                <div class="row">
-                    <div class="col-sm-8">
-                        <div class="tab">
-                            <button class="tablinks" onclick="openCity(event, 'Rates')">Rates</button>
-                            <button class="tablinks" onclick="openCity(event, 'Inclusion')">Inclusion</button>
-                        </div>
-
-                        <div id="Rates" class="tabcontent">
-                            <h3>Rates</h3>
-                            <p>London is the capital city of England.</p>
-                        </div>
-
-                        <div id="Inclusion" class="tabcontent">
-                            <h3>Inclusion</h3>
-                            <p>Paris is the capital of France.</p>
-                        </div>
+                            <input type="number" placeholder="child" id='boxIT' class='form-control form-control-lg'>
 
                     </div>
-
-                </div>
-            </div>
-        </div>
-
-
-        <div class="banner mt-5">
-            <div class="content" data-aos="zoom-in-up" data-aos-delay="300">
-                <span>start your adventures</span>
-                <h3>Let's Explore This World</h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
-                    voluptatum praesentium amet quibusdam quam officia suscipit odio.
-                </p>
-                <a href="#book-form" class="btn">book now</a>
-            </div>
-        </div>
-
-        <!-- banner section ends -->
-
-
-        <section class="footer">
-
-            <div class="box-container">
-
-                <div class="box" data-aos="fade-up" data-aos-delay="150">
-                    <a href="#" class="logo"> <i class="fas fa-paper-plane"></i>travel </a>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, ad?</p>
-                    <div class="share">
-                        <a href="#" class="fab fa-facebook-f"></a>
-                        <a href="#" class="fab fa-twitter"></a>
-                        <a href="#" class="fab fa-instagram"></a>
-                        <a href="#" class="fab fa-linkedin"></a>
+                    <div>
+                        <div>
+                            <label class='text-white' id='font'>Check-In</label>
+                            <input type="date" placeholder="mm/dd/yyyy" id='boxIT' class='form-control form-control-lg'>
+                        </div>
+                        <div>
+                            <label class='text-white' id='font'>Check-Out</label>
+                            <input type="date" placeholder="mm/dd/yyyy" id='boxIT' class='form-control form-control-lg'>
+                        </div>
                     </div>
-                </div>
-
-                <div class="box" data-aos="fade-up" data-aos-delay="300">
-                    <h3>quick links</h3>
-                    <a href="#home" class="links"> <i class="fas fa-arrow-right"></i> home </a>
-                    <a href="#about" class="links"> <i class="fas fa-arrow-right"></i> about </a>
-                    <a href="#destination" class="links"> <i class="fas fa-arrow-right"></i> destination </a>
-                    <a href="#services" class="links"> <i class="fas fa-arrow-right"></i> services </a>
-                    <a href="#gallery" class="links"> <i class="fas fa-arrow-right"></i> gallery </a>
-                    <a href="#blogs" class="links"> <i class="fas fa-arrow-right"></i> blogs </a>
-                </div>
-
-                <div class="box" data-aos="fade-up" data-aos-delay="450">
-                    <h3>contact info</h3>
-                    <p> <i class="fas fa-map"></i> Lipa, Batangas </p>
-                    <p> <i class="fas fa-phone"></i> +63 9082447247 </p>
-                    <p> <i class="fas fa-envelope"></i> sample@gmail.com </p>
-                    <p> <i class="fas fa-clock"></i> 7:00am - 10:00pm </p>
-                </div>
-
-                <div class="box" data-aos="fade-up" data-aos-delay="600">
-                    <h3>newsletter</h3>
-                    <p>subscribe for latest updates</p>
-                    <form action="">
-                        <input type="email" name="" placeholder="enter your email" class="email" id="">
-                        <input type="submit" value="subscribe" class="btn">
+                    <div>
+                        <textarea name="Inquiry" rows="5" class='form-control form-control-lg'
+                            placeholder='Your Message'></textarea>
+                    </div>
+                    <div>
+                        <input type='submit' id='boxIT' class='form-control form-control-lg bg-dark text-white'>
+                    </div>
                     </form>
                 </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class='container mt-4'>
+        <div class="container" id='margin'>
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="tab">
+                        <button class="tablinks" onclick="openCity(event, 'Rates')">Rates</button>
+                        <button class="tablinks" onclick="openCity(event, 'Inclusion')">Inclusion</button>
+                    </div>
+
+                    <div id="Rates" class="tabcontent">
+                        <h3 class='text-white'>Rates</h3>
+                        <p class='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque possimus reprehenderit expedita quas ducimus quos minus iure omnis, labore soluta! Ducimus sint obcaecati voluptates distinctio ratione, eum dignissimos! Temporibus, magni?</p>
+                    </div>
+
+                    <div id="Inclusion" class="tabcontent">
+                        <h3 class='text-white'>Inclusion</h3>
+                        <p class='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim consequatur nihil vero iure temporibus repudiandae quasi nisi impedit adipisci corrupti sapiente accusantium reiciendis assumenda ipsa dolores, ratione, ad, maiores quo.</p>
+                    </div>
+
+                </div>
 
             </div>
+        </div>
+    </div>
 
-        </section>
 
-        <div class="credit">created by <span>Group 2</span> | all rights reserved!</div>
+    <div class="banner mt-5">
+        <div class="content" data-aos="zoom-in-up" data-aos-delay="300">
+            <span>start your adventures</span>
+            <h3>Let's Explore This World</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+                voluptatum praesentium amet quibusdam quam officia suscipit odio.
+            </p>
+            <a href="#book-form" class="btn">book now</a>
+        </div>
+    </div>
 
-        <script>
-        function openCity(evt, cityName) {
-            var i, tabcontent, tablinks;
-            tabcontent = document.getElementsByClassName("tabcontent");
-            for (i = 0; i < tabcontent.length; i++) {
-                tabcontent[i].style.display = "none";
-            }
-            tablinks = document.getElementsByClassName("tablinks");
-            for (i = 0; i < tablinks.length; i++) {
-                tablinks[i].className = tablinks[i].className.replace(" active", "");
-            }
-            document.getElementById(cityName).style.display = "block";
-            evt.currentTarget.className += " active";
+    <!-- banner section ends -->
+
+
+    <section class="footer">
+
+        <div class="box-container">
+
+            <div class="box" data-aos="fade-up" data-aos-delay="150">
+                <a href="#" class="logo"> <i class="fas fa-paper-plane"></i>travel </a>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, ad?</p>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                </div>
+            </div>
+
+            <div class="box" data-aos="fade-up" data-aos-delay="300">
+                <h3>quick links</h3>
+                <a href="#home" class="links"> <i class="fas fa-arrow-right"></i> home </a>
+                <a href="#about" class="links"> <i class="fas fa-arrow-right"></i> about </a>
+                <a href="#destination" class="links"> <i class="fas fa-arrow-right"></i> destination </a>
+                <a href="#services" class="links"> <i class="fas fa-arrow-right"></i> services </a>
+                <a href="#gallery" class="links"> <i class="fas fa-arrow-right"></i> gallery </a>
+                <a href="#blogs" class="links"> <i class="fas fa-arrow-right"></i> blogs </a>
+            </div>
+
+            <div class="box" data-aos="fade-up" data-aos-delay="450">
+                <h3>contact info</h3>
+                <p> <i class="fas fa-map"></i> Lipa, Batangas </p>
+                <p> <i class="fas fa-phone"></i> +63 9082447247 </p>
+                <p> <i class="fas fa-envelope"></i> sample@gmail.com </p>
+                <p> <i class="fas fa-clock"></i> 7:00am - 10:00pm </p>
+            </div>
+
+            <div class="box" data-aos="fade-up" data-aos-delay="600">
+                <h3>newsletter</h3>
+                <p>subscribe for latest updates</p>
+                <form action="">
+                    <input type="email" name="" placeholder="enter your email" class="email" id="">
+                    <input type="submit" value="subscribe" class="btn">
+                </form>
+            </div>
+
+        </div>
+
+    </section>
+
+    <div class="credit">created by <span>Group 2</span> | all rights reserved!</div>
+
+    <script>
+    function openCity(evt, cityName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
         }
-        </script>
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(cityName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+    </script>
 
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
 
-        <script>
-        AOS.init({
-            duration: 800,
-            offset: 150,
-        });
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
-        </script>
+    <script>
+    AOS.init({
+        duration: 800,
+        offset: 150,
+    });
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
